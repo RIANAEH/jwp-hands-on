c78501deff24144bb309f6a9b057d6b372189929
RIANAEH <ririhan217@gmail.com>
2022-09-16 17:20:00 +0900

    feat: Reflections 학습 테스트 완료

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,10 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.lang.annotation.Annotation;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -10,9 +15,22 @@ class ReflectionsTest {
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
+        /*
+            Reflections 클래스는 원하는 클래스를 찾기 위해 사용한다.
+            prefix는 클래스를 찾을 때 출발 패키지를 의미한다.
+         */
         Reflections reflections = new Reflections("examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        // 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        List.of(Controller.class, Service.class, Repository.class)
+                .forEach(a -> searchAndPrintWithAnnotation(reflections, a));
+    }
+
+    private void searchAndPrintWithAnnotation(Reflections reflections, Class<? extends Annotation> annotation) {
+        reflections.getTypesAnnotatedWith(annotation)
+                .stream()
+                .map(Class::getSimpleName)
+                .forEach(log::info);
     }
 }
