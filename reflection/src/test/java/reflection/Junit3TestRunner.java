3046dd9c8757e7e40b5ca6d3fbcabda5c6f98fe6
RIANAEH <ririhan217@gmail.com>
2022-09-16 16:36:00 +0900

    feat: Junit3 학습 테스트 완료

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,25 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
+        // Junit3Test.clss의 Class 생성
         Class<Junit3Test> clazz = Junit3Test.class;
+        // Class로 기본 생성자 생성
+        Constructor<Junit3Test> constructor = clazz.getDeclaredConstructor();
+        // 기본 생성자로 객제 생성
+        Junit3Test junit3Test = constructor.newInstance();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        // Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
