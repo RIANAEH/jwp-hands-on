4139af7c7c985cf9b4e34f629402eb9eafafb3cf
RIANAEH <ririhan217@gmail.com>
2022-09-16 15:52:27 +0900

    feat: 필터 학습 테스트 완료

--- servlet/src/main/java/com/example/CharacterEncodingFilter.java
+++ servlet/src/main/java/com/example/CharacterEncodingFilter.java
@@ -11,6 +11,10 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+
+        // 서블릿에서 데이터 인코딩을 utf-8로 설정
+        response.setCharacterEncoding("utf-8");
+
         chain.doFilter(request, response);
     }
 }
